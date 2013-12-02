53aaf1a300316d4c31d80195628d9e1fba61142a
Federico Di Nucci <dinu1389@gmail.com>
2013-12-02 16:08:56 -0300

    Read con Cache

--- src/dfs/DFSCliente.java
+++ src/dfs/DFSCliente.java
@@ -2,15 +2,20 @@
 
 package dfs;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DFSCliente {
 
 	DFSServicio serv;
-	Cache cache;
+	Map<String, Cache> caches = new HashMap<String, Cache>();
+	int tamCache;
+	int tamBloque;
 
 	public DFSCliente(int tamBloque, int tamCache)
 			throws MalformedURLException, RemoteException, NotBoundException {
@@ -19,10 +24,37 @@ public class DFSCliente {
 		String puerto = System.getenv("PUERTO");
 		this.serv = (DFSServicio) Naming.lookup("rmi://" + servidor + ":"
 				+ puerto + "/DFS");
-		cache = new Cache(tamCache);
+		this.tamCache = tamCache;
+		this.tamBloque = tamBloque;
+	}
+
+	public int getTamCache() {
+		return tamCache;
+	}
+
+	public int getTamBloque() {
+		return tamBloque;
 	}
 
 	public DFSServicio getServidor() {
 		return serv;
+	}
+
+	public DFSFicheroServ open(String nom, String modo, Double usuario)
+			throws IOException {
+		DFSFicheroServ resp = serv.open(nom, modo, usuario);
+
+		return resp;
+
+	}
+
+	public Cache getCache(String nom) {
+		Cache resp;
+		if (!caches.containsKey(nom)) {
+			resp = new Cache(tamCache);
+			caches.put(nom, resp);
+		} else
+			resp = caches.get(nom);
+		return resp;
 	}
 }
--- src/dfs/DFSFicheroCliente.java
+++ src/dfs/DFSFicheroCliente.java
@@ -2,44 +2,100 @@
 
 package dfs;
 
-import java.io.*;
-import java.rmi.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.RemoteException;
 
 public class DFSFicheroCliente {
 
-	DFSFicheroServ fich;
-	Double usuario;
+	private DFSFicheroServ fich;
+	private Double usuario;
+	private DFSCliente dfs;
+	private String nombre;
+	private long puntero = 0;
+	private Cache cache;
+	private int tamBloque;
 
 	public DFSFicheroCliente(DFSCliente dfs, String nom, String modo)
 			throws RemoteException, IOException, FileNotFoundException {
 
 		usuario = Math.random();
-		fich = dfs.getServidor().open(nom, modo, usuario);
+		fich = dfs.open(nom, modo, usuario);
+		this.nombre = nom;
+		this.dfs = dfs;
+		cache = dfs.getCache(nom);
+		tamBloque = dfs.getTamBloque();
 
 	}
 
 	public int read(byte[] b) throws RemoteException, IOException {
 
-		byte[] resp = fich.read(b.length);
-		if (resp == null)
-			return -1;
-		System.arraycopy(resp, 0, b, 0, resp.length);
+		int cantBloques = b.length / tamBloque;
+		int bloqueInicial = (int) (puntero / tamBloque);
+		int leidos = 0;
+		int i = 0;
+		Bloque bloque;
+		for (; i < cantBloques && leidos != -1; i++) {
+			byte leido[] = new byte[tamBloque];
+			if ((bloque = cache.getBloque(bloqueInicial + i)) == null) {
+				leidos = readInter(leido, puntero + (i * tamBloque));
+				if (leidos != -1) {
+					System.out.println("METO EN LA CACHE BLOQUE"
+							+ (bloqueInicial + i));
+					System.out.println(new String(leido));
+					bloque = new Bloque((bloqueInicial + i), leido);
+					cache.putBloque(bloque);
+				}
+			}
+			if (leidos != -1) {
+				byte[] info = bloque.obtenerContenido();
+				System.arraycopy(info, 0, b, i * tamBloque, info.length);
+			}
+		}
+
+		incrementarPuntero(i * tamBloque);
+
 		return b.length;
+
 	}
 
 	public void write(byte[] b) throws RemoteException, IOException {
 
 		fich.write(b);
+		incrementarPuntero(b.length);
 
 	}
 
 	public void seek(long p) throws RemoteException, IOException {
 
 		fich.seek(p);
+		setearPuntero(p);
 	}
 
 	public void close() throws RemoteException, IOException {
 
 		fich.close();
+	}
+
+	private void incrementarPuntero(long length) {
+		this.puntero += length;
+	}
+
+	private void setearPuntero(long p) {
+		this.puntero = p;
+	}
+
+	private int readInter(byte[] b, long from) throws RemoteException,
+			IOException {
+
+		fich.seek(from);
+		byte[] resp = fich.read(tamBloque);
+		if (resp == null) {
+			return -1;
+		}
+		System.arraycopy(resp, 0, b, 0, tamBloque);
+		System.out.println("Leo desde " + from + " " + new String(b));
+		return b.length;
+
 	}
 }
